d2ce505a0f36d05e1c96e412158cbc2e0cbf3a22
hassan <davidlopezolivares05@gmail.com>
2026-04-05 10:45:36 -0400

    correccion cors

--- src/main/java/com/back/puntoventa/app/config/CorsConfig.java
+++ src/main/java/com/back/puntoventa/app/config/CorsConfig.java
@@ -7,16 +7,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
-
-@Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins(
-                    "http://localhost:4200",
-                    "https://front-puntoventa.onrender.com"
-                )
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                            "http://localhost:4200", 
+                            "https://front-puntoventa.onrender.com"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }

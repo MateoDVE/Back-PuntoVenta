ff68b2d390857cf0ada489ead36ad48ebedb1554
hassan <davidlopezolivares05@gmail.com>
2026-04-15 18:17:56 -0400

    reestructurando el pom

--- src/main/java/com/back/puntoventa/app/config/CorsConfig.java
+++ src/main/java/com/back/puntoventa/app/config/CorsConfig.java
@@ -7,15 +7,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    @Override
+    // Quitamos @Override si sigue dando error de compilación en Render
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
                     "http://localhost:4200",
                     "https://front-puntoventa.onrender.com"
-
                 )
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*") // Importante para que pasen los tokens/JSON
                 .allowCredentials(true);
     }
 }

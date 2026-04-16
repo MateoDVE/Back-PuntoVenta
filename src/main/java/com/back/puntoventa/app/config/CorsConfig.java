ecb1e8fa5ead77d448c7fe57852a6d3dccbb24d1
hassan <davidlopezolivares05@gmail.com>
2026-04-15 20:18:28 -0400

    cors modificado

--- src/main/java/com/back/puntoventa/app/config/CorsConfig.java
+++ src/main/java/com/back/puntoventa/app/config/CorsConfig.java
@@ -1,22 +1,23 @@
 package com.back.puntoventa.app.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(
+                .allowedOriginPatterns(
                     "http://localhost:4200",
-                    "https://front-puntoventa.onrender.com"
+                    "https://front-puntoventa.onrender.com",
+                    "https://*.onrender.com" // Esto permite cualquier subdominio de Render
                 )
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-                .allowedHeaders("*") // IMPORTANTE: Permite todos los headers (Content-Type, Authorization, etc.)
+                .allowedHeaders("*")
                 .allowCredentials(true)
-                .maxAge(3600); // Cachea la respuesta del CORS por una hora
+                .maxAge(3600);
     }
 }

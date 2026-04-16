29970594d51947465f394ef52339f915cc8335e5
hassan <davidlopezolivares05@gmail.com>
2026-04-16 18:02:19 -0400

    fix: change url front

--- src/main/java/com/back/puntoventa/app/config/CorsConfig.java
+++ src/main/java/com/back/puntoventa/app/config/CorsConfig.java
@@ -13,7 +13,8 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowedOriginPatterns(
                     "http://localhost:4200",
                     "https://front-puntoventa.onrender.com",
-                    "https://*.onrender.com" // Esto permite cualquier subdominio de Render
+                    "https://*.onrender.com", // Esto permite cualquier subdominio de Render
+                    "https://front-punto-venta.vercel.app"
                 )
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
